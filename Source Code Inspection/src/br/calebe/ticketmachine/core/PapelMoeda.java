e3246105d9fad9e6b583481ebbdc3e7ed9d8ef4d
31413031 <31413031@092904-P31S401.macklabs.br>
2015-09-23 11:56:19 -0300

    Alterado para public

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -7,7 +7,7 @@ package br.calebe.ticketmachine.core;
 public class PapelMoeda {
 
     public int valor;
-    protected int quantidade;
+    public int quantidade;
 
     public PapelMoeda(int valor, int quantidade) {
         this.valor = valor;
